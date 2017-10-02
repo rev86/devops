f4615a0fc85ef4bdf96f4dbedf674b41b738ef70
eratush <positivfoto@gmail.com>
2017-10-02 17:01:13 +0300

    initial

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  #access_key = "ACCESS_KEY_HERE"
+  #secret_key = "SECRET_KEY_HERE"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "amzn-test" {
+  ami           = "ami-4fffc834"
+  instance_type = "t2.micro"
+  key_name      = "test"
+}
